146312a1a0bf5421ef0b3ab9d5cad950d01d7105
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 17:25:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,9 @@ class Transfer
     self.receiver.balance += amount
     self.sender.balance -= amount
     self.status = "complete"
+    elsif
+      self.sender.balance < amount
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
